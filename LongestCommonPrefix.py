fc9fe304c8117dc1e6ab671b41bb766348710bbe
Veronika Khan <vkhan@seas.upenn.edu>
2023-05-20 21:16:26 +0800

    Single function + fix

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -6,29 +6,28 @@
 # strs[i] consists of only lowercase English letters.
 
 def longestCommonPrefix(strs):
-    res = ""
-    common = ""
-    minLen = shortestStr(strs)
+    minLen = 201
+    for i in strs:
+        if 0 <= len(i) <= 200 and len(i) < minLen:
+            minLen = len(i)
 
+    res = ""
     if 1 <= len(strs) <= 200 and minLen <= 200:
         for i in range(minLen):
             common = strs[0][i]
-            flag = 1
+            flag = True
             for s in range(1, len(strs)):
                 if strs[s][i] != common:
-                    flag = 0
-            if flag == 1:
-                res = res + common
+                    flag = False
+                    return res
+            res = res + common
     return res
-
-def shortestStr(strs):
-    l = 201
-    for i in strs:
-        if 0 <= len(i) <= 200 and len(i) < l:
-            l = len(i)
-    return l
 
 resStr1 = longestCommonPrefix(["flower","flow","flight"])
 print(resStr1)
 resStr2 = longestCommonPrefix(["dog","racecar","car"])
 print(resStr2)
+resStr3 = longestCommonPrefix(["carpet", "car", "cannon"])
+print(resStr3)
+resStr4 = longestCommonPrefix(["cir", "car"])
+print(resStr4)
