5df71ccb34b6b3a649c45eac644304078706b43a
Veronika Khan <vkhan@seas.upenn.edu>
2023-05-25 07:17:46 +0800

    Reverse Polish notation + fix

--- ValidParenthesis.py
+++ ValidParenthesis.py
@@ -10,19 +10,20 @@
 def isValid(s):
     d = {"(": ")", "{": "}", "[": "]"}
     stack = []
-    flag = True
     if len(s) % 2 == 1 or s[0] in d.values() or s[len(s)-1] in d.keys() or 1 > len(s) > 104:
-        flag = False
+        return False
     else:
         for i in s:
             if i in d.keys():
                 stack.append(d[i])
             if i in d.values():
-                if i != stack.pop():
-                    flag = False
-                    return flag
-    return flag
+                if len(stack) != 0 and i != stack.pop():
+                    return False
 
+    if len(stack) == 0:
+        return True
+    else:
+        return False
 
 r1 = isValid("()")
 print(r1)
@@ -38,3 +39,7 @@ r6 = isValid("){")
 print(r6)
 r7 = isValid("({{{{}}}))")
 print(r7)
+r8 = isValid("[[[]")
+print(r8)
+r9 = isValid("()))")
+print(r9)
